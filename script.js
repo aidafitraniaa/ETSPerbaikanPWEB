d797c5445daa77084f8257c826e7ea09fd692f6c
Aida Fitrania <91545802+aidafitraniaa@users.noreply.github.com>
2023-11-08 14:27:20 +0700

    2 kolom

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch("https://pokeapi.co/api/v2/pokemon")
     .then((response) => {
       return response.json();
     })
-
+    
     .then((response) => {
       console.log(response);
       const data = response.results;
@@ -10,7 +10,7 @@ fetch("https://pokeapi.co/api/v2/pokemon")
       data.forEach((pok) => {
         cards += `
 
-        <div class="col-xl-3 col-md-6 my-4">
+        <div class="col-xl-6 col-md-6 my-4 col-6">
           <div class="card border-primary shadow h-100 border-primary blue" style="max-width: 30rem">
             <div class="card-body text-primary text-center">
               <p class="h5 mb-2 card-text text-dark">
@@ -19,9 +19,9 @@ fetch("https://pokeapi.co/api/v2/pokemon")
               <a href="${pok.url}" class="btn btn-primary">Detail</a>
             </div>
           </div>
-      
-      </div>`;
+
+        </div>`;
       });
       const pokemonContainer = document.querySelector("#card");
-      pokemonContainer.innerHTML = cards;
+      pokemonContainer.innerHTML = cards;
     });
